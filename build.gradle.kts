b96435358c4ca50018e459023343911159b3b541
suryatejagajjela <gajjelasuryateja2021@gmail.com>
2026-03-13 10:31:33 -0400

    Fresh initial commit with all files

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("java")
+    id("org.jetbrains.intellij") version "1.17.2"
+}
+
+group = "com.blastradius"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Basic dependencies for JSON/HTTP if needed
+    implementation("org.json:json:20231013")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+}
+
+intellij {
+    version.set("2023.2.5")
+    type.set("IC") // IntelliJ IDEA Community Edition
+
+    plugins.set(listOf("com.intellij.java", "Git4Idea", "org.jetbrains.plugins.github"))
+}
+
+tasks {
+    withType<JavaCompile> {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+
+    patchPluginXml {
+        sinceBuild.set("232")
+        untilBuild.set("242.*")
+    }
+}
